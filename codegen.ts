7877a7a41373db6128dddf36ed41e73d8133adc5
bartektricks <bartektricks@gmail.com>
2024-05-15 14:43:11 +0200

    fix: codegen document paths

--- codegen.ts
+++ codegen.ts
@@ -11,7 +11,7 @@ const config: CodegenConfig = {
       },
     },
   },
-  documents: ['app/**/*.tsx'],
+  documents: ['app/**/*.{ts,tsx}'],
   overwrite: true,
   ignoreNoDocuments: true, // for better experience with the watcher
   generates: {
