326c0431dfad583b870692534fdbe1720a2c8eeb
bartektricks <bartektricks@gmail.com>
2024-05-15 14:43:59 +0200

    chore: add debouncing and update status codes

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,13 @@
-import type { LoaderFunction } from '@remix-run/node';
-import { type MetaFunction, json, useLoaderData } from '@remix-run/react';
+import type { LoaderFunctionArgs } from '@remix-run/node';
+import {
+  type MetaFunction,
+  json,
+  useLoaderData,
+  useSubmit,
+} from '@remix-run/react';
+import { debounce } from 'radash';
 import { getSearchResults } from '~/libs/api/search.server';
+import statusCodes from '~/utils/statusCodes.server';
 
 export const meta: MetaFunction = () => {
   return [
@@ -9,21 +16,45 @@ export const meta: MetaFunction = () => {
   ];
 };
 
-export const loader: LoaderFunction = async ({ request }) => {
-  const url = new URL(request.url);
-  const q = url.searchParams.get('q') ?? 'michal';
+const DEFAULT_QUERY = 'bartek';
 
-  const res = await getSearchResults(q);
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  const q = url.searchParams.get('q') ?? '';
+
+  const res = await getSearchResults(q ?? DEFAULT_QUERY);
 
   if (res.error ?? !res.data) {
-    throw json(`Failed to fetch query: ${q}`, { status: 404 });
+    throw json(`Failed to fetch query: ${q ?? DEFAULT_QUERY}`, {
+      status: statusCodes.HTTP_STATUS_NOT_FOUND,
+    });
   }
 
-  return json(res.data, { status: 200 });
+  return json({ data: res.data, q }, { status: statusCodes.HTTP_STATUS_OK });
 };
 
 export default function Index() {
-  const data = useLoaderData<typeof loader>();
+  const { data, q } = useLoaderData<typeof loader>();
+  const submit = useSubmit();
 
-  return <pre>{JSON.stringify(data, null, 2)}</pre>;
+  const debouncedSubmit = debounce<[e: string]>({ delay: 220 }, (value) => {
+    submit({ q: value });
+  });
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    debouncedSubmit(e.target.value);
+  };
+
+  return (
+    <div>
+      <input
+        type="search"
+        name="q"
+        placeholder="Search"
+        onChange={onChange}
+        defaultValue={q}
+      />
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
 }
