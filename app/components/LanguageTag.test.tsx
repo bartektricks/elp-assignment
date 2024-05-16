0240b9b0e9c8d39736b21a5919d1664f407473a1
bartektricks <bartektricks@gmail.com>
2024-05-16 16:45:56 +0200

    test: add unit tests for LanguageTag component

---
+++ app/components/LanguageTag.test.tsx
@@ -0,0 +1,30 @@
+import { render, screen } from '@testing-library/react';
+import LanguageTag from './LanguageTag';
+
+const NAME = 'TypeScript';
+
+describe('LanguageTag', () => {
+  test('should render a name and default color', () => {
+    render(<LanguageTag name={NAME} />);
+
+    expect(screen.getByText(NAME)).toBeInTheDocument();
+
+    expect(screen.getByTestId('language-tag-color')).toHaveClass('bg-blue');
+    expect(screen.getByTestId('language-tag-color')).toHaveStyle({
+      backgroundColor: undefined,
+    });
+  });
+
+  test('should render a name and set color', () => {
+    const color = '#8a2be2';
+
+    render(<LanguageTag name={NAME} color={color} />);
+
+    expect(screen.getByText(NAME)).toBeInTheDocument();
+
+    expect(screen.getByTestId('language-tag-color')).toHaveClass('bg-blue');
+    expect(screen.getByTestId('language-tag-color')).toHaveStyle({
+      backgroundColor: color,
+    });
+  });
+});
--- app/components/LanguageTag.tsx
+++ app/components/LanguageTag.tsx
@@ -7,6 +7,7 @@ export default function LanguageTag({ name, color }: LanguageTagProps) {
   return (
     <span className="flex items-center gap-1">
       <div
+        data-testid="language-tag-color"
         role="presentation"
         className="size-3 rounded-full bg-blue"
         style={{
