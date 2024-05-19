6bb95dec75097b45e471b69dcd6d89a00df3e7f5
bartektricks <bartektricks@gmail.com>
2024-05-19 12:13:08 +0200

    test: install MSW and test search api

--- __tests__/search.test.ts
+++ __tests__/search.test.ts
@@ -1,16 +1,59 @@
 // @vitest-environment node
 
-import { getSearchResults } from '../app/libs/api/search.server';
+import {
+  QUERIES_PER_PAGE,
+  getBase64EncodedCursor,
+  getSearchResults,
+} from '../app/libs/api/search.server';
 
-// TODO: maybe could have also a mocked scenario
+import { HttpResponse, graphql } from 'msw';
+import { setupServer } from 'msw/node';
+
+const QUERY = 'test';
+
+// Mock to not hit the Github API
+setupServer(
+  graphql.query('Search', () => {
+    return HttpResponse.json({
+      data: {
+        test: 'mocked data',
+      },
+    });
+  }),
+).listen();
+
 describe('getSearchResults', () => {
-  it('should request github api', async () => {
-    const query = 'example';
+  it('should request github api and show first 5 elements', async () => {
+    const results = await getSearchResults(QUERY);
 
-    const results = await getSearchResults(query);
+    expect(results.operation.context.url).toContain('github.com/graphql');
+    expect(results.operation.variables.first).toBe(5);
+  });
 
-    expect(results.operation.context.url).toContain('github');
-    expect(results.operation.variables.repoQuery).toBe(query);
-    expect(results.operation.variables.userQuery).toBe(`${query} type:user`);
+  it('cursors should be null by default', async () => {
+    const results = await getSearchResults(QUERY);
+
+    expect(results.operation.variables.repoCursor).toBeNull();
+    expect(results.operation.variables.userCursor).toBeNull();
+  });
+
+  it('cursors should be a base64 value of cursor:25 when page number is 5', async () => {
+    const cursorValue = getBase64EncodedCursor(5 * QUERIES_PER_PAGE);
+    const results = await getSearchResults(QUERY, 5);
+
+    expect(results.operation.variables.repoCursor).toBe(cursorValue);
+    expect(results.operation.variables.userCursor).toBe(cursorValue);
+  });
+
+  it('cursors should be NULL if a page number is below or equal to 0', async () => {
+    const results = await getSearchResults(QUERY, -5);
+
+    expect(results.operation.variables.repoCursor).toBeNull();
+    expect(results.operation.variables.userCursor).toBeNull();
+
+    const results2 = await getSearchResults(QUERY, 0);
+
+    expect(results2.operation.variables.repoCursor).toBeNull();
+    expect(results2.operation.variables.userCursor).toBeNull();
   });
 });
--- app/libs/api/search.server.ts
+++ app/libs/api/search.server.ts
@@ -36,7 +36,7 @@ const SearchQuery = graphql(`
   }
 `);
 
-function getBase64EncodedCursor(cursorNumber: number) {
+export function getBase64EncodedCursor(cursorNumber: number) {
   return Buffer.from(`cursor:${cursorNumber}`).toString('base64');
 }
 
@@ -44,7 +44,7 @@ function getBase64EncodedCursor(cursorNumber: number) {
 // which means that if one is missing then the maximum number of results will be at least half the size.
 // Since the query is cached I could do an additional check and adjust the size accordingly but I'm too lazy.
 export function getSearchResults(query: string, pageNumber = 0) {
-  const queryPageNumber = Math.abs(pageNumber) * QUERIES_PER_PAGE;
+  const queryPageNumber = pageNumber * QUERIES_PER_PAGE;
   const cursor =
     queryPageNumber > 0 ? getBase64EncodedCursor(queryPageNumber) : null;
 
